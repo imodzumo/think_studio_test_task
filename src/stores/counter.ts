743e0572c86683e3a9412cfe33db0f22293dc88b
igor <igor.angelchev@tateeda.com>
2025-05-04 23:49:58 +0300

    some fixes

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,7 +3,6 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
   const increment = () => count.value++
   const decrement = () => count.value--
 
